4d41b131358140059de4e812964f42c3911389c2
cheskyposen <shragaposen@gmail.com>
2019-05-15 14:51:43 -0400

    making it custom

--- web/index.php
+++ web/index.php
@@ -4,8 +4,9 @@
     // gets an associative array of all headers in the http request
     $GLOBALS['headers'] = apache_request_headers();
     // includes all required libraries for the api
-    require_once '../app/bootstrap.php';
+    //require_once '../app/bootstrap.php';
     // Init Core Library
-    $init = new Core;
+    //$init = new Core;
     // un sets global vars
     unset($GLOBALS);
+    return json_encode(['msg' => 'welcome to our app']);
