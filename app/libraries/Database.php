f58b5c1e1105a955140ba43d15c678ba0c4f36ea
cheskyposen <shragaposen@gmail.com>
2019-05-15 15:30:48 -0400

    trying to fix db

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -7,10 +7,10 @@
    * Return rows and results
    */
   class Database {
-    private $host = DB_HOST;
-    private $user = DB_USER;
-    private $pass = DB_PASS;
-    private $dbname = DB_NAME;
+//    private $host = DB_HOST;
+//    private $user = DB_USER;
+//    private $pass = DB_PASS;
+//    private $dbname = DB_NAME;
 
     private $dbh;
     private $stmt;
@@ -18,15 +18,25 @@
 
     public function __construct(){
       // Set DSN
-      $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-      $options = array(
-        PDO::ATTR_PERSISTENT => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      );
+     // $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+//      $options = array(
+//        PDO::ATTR_PERSISTENT => true,
+//        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+//      );
 
       // Create PDO instance
       try{
-        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+       // $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+          $db = parse_url(getenv("DATABASE_URL"));
+
+          $this->dbh = new PDO("pgsql:" . sprintf(
+                  "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                  $db["host"],
+                  $db["port"],
+                  $db["user"],
+                  $db["pass"],
+                  ltrim($db["path"], "/")
+              ));
       } catch(PDOException $e){
         $this->error = $e->getMessage();
         echo $this->error;
