e6c3aebb03068152353bf7f5046bca48705177dc
cheskyposen <shragaposen@gmail.com>
2019-05-17 14:20:45 -0400

    working on token auth fix

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -19,7 +19,9 @@ class Controller {
         $db->query('SELECT * FROM auth WHERE token = :token AND expiry > now()');
         $db->bind(':token', $token);
         // check database if token exists and not expired
-        if ($res = $db->single()){
+        $res = $db->single();
+        echo json_encode($res);
+        if ($res){
             echo json_encode($res);
             Controller::updateTokenExpiry($token);
             // checks if token matches to ip address
