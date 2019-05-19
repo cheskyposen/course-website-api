014b56bd2a9374a80396da418c2d8c23ad774e12
cheskyposen <shragaposen@gmail.com>
2019-05-19 13:47:08 -0400

    changed headers

--- web/index.php
+++ web/index.php
@@ -12,15 +12,8 @@
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400');    // cache for 1 day
             // Access-Control headers are received during OPTIONS requests
-            if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-                    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-                    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-            }
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+            header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
         }
     }
     // gets an associative array of all headers in the http request
